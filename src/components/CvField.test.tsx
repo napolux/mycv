2283d4c6b175682b6f109545b214790c1f51fd63
Napolux <napolux@gmail.com>
2020-03-29 14:53:36 +0200

    Test fix

--- src/components/CvField.test.tsx
+++ src/components/CvField.test.tsx
@@ -11,7 +11,7 @@ it('renders correctly the name field', () => {
 
 it('renders correctly the date field', () => {
     const tree = renderer
-      .create(<CvField cvFieldName="dob" cvFieldValue="2018" />)
+      .create(<CvField cvFieldName="dob" cvFieldValue="2012-12-12" />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
